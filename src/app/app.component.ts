bed5a80a46c25ead7c8c26898faed54b72f76cf4
Andrei Dailida <2890370@gmail.com>
2019-01-25 17:31:07 +0100

    WBD. Projekt 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
+export interface Boss {
+  name: string;
+  surname: string;
+  date;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'club';
+  private bosses: Boss[] = [{name: 'andrzej', surname: 'dailida', date: '25.01.19'}];
+
+  onSubmit(name: string, surname: string, date) {
+    const boss: Boss = {name: name, surname: surname, date: date};
+    this.bosses.push(boss); // tutaj wysyłanie do bd po http post
+  }
+
+
+  delete(name: string, surname: string) {
+    console.log('name' + name);
+    console.log('surname' + surname);
+    const boss = this.bosses.find(item => item.surname === surname && item.name === name);
+    console.log(boss);
+
+    const index = this.bosses.indexOf(boss);
+    console.log(index);
+    console.log(this.bosses);
+    this.bosses.splice(index, 1);
+    console.log(this.bosses);
+  }
 }
